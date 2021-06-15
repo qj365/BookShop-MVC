bde104b1202a2f8685dee48d5f5f83dc259671fe
QJ <quangjunior365@gmail.com>
2021-06-15 17:35:36 +0700

    them order

---
+++ BookShop/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using BookShop.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using PagedList;
+
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index(int? pageNum, string name = null, string email = null, string sdt = null, string username = null)
+        {
+            if (name == null && email == null && sdt == null && username == null)
+                return View("Search");
+
+            IQueryable<Customer> customerQuery = _context.Customers;
+
+            if (!String.IsNullOrWhiteSpace(name))
+                customerQuery = customerQuery.Where(c => c.Name.Contains(name));
+
+            if (!String.IsNullOrWhiteSpace(email))
+                customerQuery = customerQuery.Where(c => c.Email.Contains(email));
+
+            if (!String.IsNullOrWhiteSpace(sdt))
+                customerQuery = customerQuery.Where(c => c.Sdt.Contains(sdt));
+
+            if (!String.IsNullOrWhiteSpace(username))
+                customerQuery = customerQuery.Where(c => c.Username.Contains(username));
+
+            var customer = customerQuery.ToList();
+            return View("Index",customer.ToPagedList(pageNum ?? 1, 5));
+        }
+    }
+}
---
+++ BookShop/Areas/Admin/Controllers/DeliveryController.cs
@@ -0,0 +1,30 @@
+﻿using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public DeliveryController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var order = _context.Orders.Include(c => c.Customer);
+            return View();
+        }
+    }
+}
