672f1a0792facfae7e04aab3cc5b1c5124b00858
Zyr0nX <55247256+Zyr0nX@users.noreply.github.com>
2021-06-16 22:31:40 +0700

    Hiển thị trang Product

--- BookShop/App_Start/RouteConfig.cs
+++ BookShop/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace BookShop
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Product",
+                url: "{metatitle}-{id}",
+                defaults: new { controller = "Product", action = "ProductDetail", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional}
             );
+
+            
         }
     }
 }
---
+++ BookShop/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetail(int id)
+        {
+            var model = from book in _context.Books
+                        where book.Id == id
+                        select book;
+            return View(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult RenderProductContent(int id)
+        {
+            var model = from book in _context.Books
+                        where book.Id == id
+                        select book;
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult RenderProductDetail(int id)
+        {
+            var model = from book in _context.Books
+                        where book.Id == id
+                        select book;
+            return PartialView(model);
+        }
+    }
+}
